a13b731fdae5e072ef0130ea91e56961f908e20d
Khadesha McLeish <khadesham@gmail.com>
2015-03-18 16:26:10 -0400

    Update Algorithm.cc

--- Algorithm.cc
+++ Algorithm.cc
@@ -1,12 +1,6 @@
-#include<stdio.h>
-#include"Sensors.h"
-#include"data.h"
-#include"Motor.h"
-#include"Cell.h"
-#include"Wheel.h"
+#include <stdio.h>
+#include "Algorithm.h"
 
-using namespace std;
-
-    char chooseDirection(); //choose the direction to go based on the possible open spaces in the maze
-    char game_state; //variable to hold the state of the game we are in, ex: discover, speed_run, end, out_of_time 
+char chooseDirection(); //choose the direction to go based on the possible open spaces in the maze
+char game_state; //variable to hold the state of the game we are in, ex: discover, speed_run, end, out_of_time
    
